2cad492803023e6be9462fab38731fb3eb05ba04
hollisc <hollisc@ca.ibm.com>
2020-02-10 13:10:43 -0500

    Sync with source repo

--- integration/mq/setup.sh
+++ integration/mq/setup.sh
@@ -2,8 +2,10 @@
 
 PROJECT=mq
 
+oc create -f mq.yaml
+oc create -f mq-role.yaml
+oc create -f mq-role-binding.yaml
+
 oc project $PROJECT
 oc -n $PROJECT create rolebinding $PROJECT-security-policy --clusterrole=ibm-anyuid-hostpath-scc --group=system:serviceaccounts:$PROJECT
 
-oc apply -f mq-role.yaml
-oc apply -f mq-role-binding.yaml
